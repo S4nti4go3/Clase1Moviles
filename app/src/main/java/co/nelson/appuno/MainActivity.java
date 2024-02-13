e68cfd0c5fd6e5949f1811106ae024ce690b48af
S4nti4go3 <santiago.correa883@pascualbravo.edu.co>
2024-02-13 09:41:30 -0500

    Clase 3
    Creacion de ciclo de vida

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -1,8 +1,10 @@
 package co.nelson.appuno;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.DragStartHelper;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
    Button btn1,btn2;
+
+   String Tag = "Prueba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 nombreUsuario.setText("Presioné Boton Uno");
-
             }
         });
 
@@ -36,12 +39,37 @@ public class MainActivity extends AppCompatActivity {
                 nombreUsuario.setText("Presioné Boton Dos");
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i (Tag, "Esto en Onstart" );
 
     }
 
-    public double calcularArea(int l1,int l2){
-        return l1*l2;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag, "Estoy en OnResume" );
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag, "Estoy en OnPause" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag, "Estoy en OnStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,  "Estoy en OnDestroy" );
+    }
 }
